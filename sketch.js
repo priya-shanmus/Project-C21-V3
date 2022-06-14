0d5d6c2a8872f9f3e5b23bfc7497bb0c9cd8ce28
priya-shanmus <70858619+priya-shanmus@users.noreply.github.com>
2022-06-14 14:43:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,16 +14,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0,
-		density:1.2
-	}
-
-	ball = Bodies.circle(260,100,radius/2,ball_options);
-	World.add(world,ball);
-
+	
 	groundObj=new ground(width/2,670,width,20);
 	leftSide = new ground(1100,600,20,120);
 	rightSide = new ground(1350,600,20,120);
@@ -38,7 +29,7 @@ function draw() {
   background(0);
 
 
-  ellipse(ball.position.x,ball.position.y,radius,radius);
+ 
 
   groundObj.display();
   leftSide.display();  
@@ -47,9 +38,5 @@ function draw() {
 }
 
 function keyPressed() {
-  	if (keyCode === UP_ARROW) {
-
-		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
-    
-  	}
+  	
 }
